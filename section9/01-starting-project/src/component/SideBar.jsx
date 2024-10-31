6f3732672f5ccbf00f9a7750d698f60d685ca097
kkh041124 <aa01091838192@gmail.com>
2024-10-31 16:07:00 +0900

    Update : edit_sidebar state

--- section9/01-starting-project/src/component/SideBar.jsx
+++ section9/01-starting-project/src/component/SideBar.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./SideBar.module.css";
 
-const SideBar = () => {
+const SideBar = ({ title, isCheck }) => {
   const navigate = useNavigate();
   const navigatePage = () => {
     navigate("/edit");
@@ -10,6 +10,7 @@ const SideBar = () => {
     <div className={styles.SideBar}>
       <h2>YOUR PROJECT</h2>
       <button onClick={navigatePage}>+ Add Project</button>
+      <h2>{isCheck && title}</h2>
     </div>
   );
 };
--- section9/01-starting-project/src/page/Edit.jsx
+++ section9/01-starting-project/src/page/Edit.jsx
@@ -1,20 +1,42 @@
+import { useState } from "react";
 import styles from "./Edit.module.css";
+import SideBar from "../component/SideBar";
+import { useNavigate } from "react-router-dom";
 
 const Edit = () => {
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputDesc, setInputDesc] = useState("");
+  const [inputDate, setInputDate] = useState("");
+  const [isCheck, setIsCheck] = useState(false);
+  const navigate = useNavigate();
+  const handleDate = (e) => {
+    setInputDate(e.target.value);
+  };
+  const handleTitle = (e) => {
+    setInputTitle(e.target.value);
+  };
+  const saveProject = () => {
+    setIsCheck(true);
+    navigate(-1);
+  };
+
   return (
     <div className={styles.Edit}>
+      <div className={styles.SideBar}>
+        <SideBar title={inputTitle} isCheck={isCheck} />
+      </div>
       <div className={styles.Edit_wrapper}>
         <div className={styles.button_wraaper}>
           <button>Cancel</button>
-          <button>Save</button>
+          <button onClick={saveProject}>Save</button>
         </div>
         <div className={styles.Data_wrapper}>
           <h2>TITEL</h2>
-          <input type="text" />
-          <h2>DESVRIPTION</h2>
+          <input onChange={handleTitle} type="text" />
+          <h2>DESCRIPTION</h2>
           <input type="text" />
           <h2>DUE DATE</h2>
-          <input type="date" />
+          <input onChange={handleDate} type="date" />
         </div>
       </div>
     </div>
